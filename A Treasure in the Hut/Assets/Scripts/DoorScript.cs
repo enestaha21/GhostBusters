10e11305af57727cae8d4a7ea3b1635013b45d47
miragee37 <240404049@ogr.kent.edu.tr>
2026-05-12 22:10:46 +0300

    Bug fixed
    The bug which is causes opening the door from everywhere has been fixed.

--- A Treasure in the Hut/Assets/Scripts/DoorScript.cs
+++ A Treasure in the Hut/Assets/Scripts/DoorScript.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    public Animator animator; 
-    public Transform player;  
-    public float distanceToOpen = 3f; 
+    public Animator animator;
+    public Transform player;
+    public float distanceToOpen = 3f;
 
     void Update()
     {
-       
+        if (player == null) return;
+
         float dist = Vector3.Distance(player.position, transform.position);
 
-        
         if (dist < distanceToOpen && Input.GetKeyDown(KeyCode.E))
         {
             bool currentState = animator.GetBool("isOpen");
